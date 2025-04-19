3de7ba3a85e1c1e67595dd9a808d8c4d63e97ada
aravindkk004 <aravind463kk30@gmail.com>
2025-04-19 12:26:28 +0530

    Link added

--- src/components/people/TopNav.jsx
+++ src/components/people/TopNav.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { MdLogout } from "react-icons/md";
 
 const TopNav = () => {
@@ -5,10 +6,12 @@ const TopNav = () => {
     <>
       <div className="flex items-center justify-between px-10 py-3 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)]">
         <h2 className="text-3xl font-bold">TNPDS</h2>
-        <button className="bg-blue-800 text-white px-5 py-2 rounded-md flex items-center">
-          <MdLogout />
-          Logout
-        </button>
+        <Link href="/">
+          <button className="bg-blue-800 text-white px-5 py-2 rounded-md flex items-center">
+            <MdLogout />
+            Logout
+          </button>
+        </Link>
       </div>
     </>
   );
